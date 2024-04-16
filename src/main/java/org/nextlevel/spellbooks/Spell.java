9ef6f0a8304daff481550d36498e2055cb39a771
NextLevel-457 <calvinjtenny@icloud.com>
2024-04-15 17:19:38 -0700

    Basic Spell class file.

--- src/main/java/org/nextlevel/spellbooks/Spell.java
+++ src/main/java/org/nextlevel/spellbooks/Spell.java
@@ -1,4 +1,14 @@
 package org.nextlevel.spellbooks;
 
 public class Spell {
+    public double range = 5.0;
+
+    public enum Form {RAYCAST, AOE, BLOCK}
+
+    public Form form;
+
+    public Spell(double range, Form form) {
+        this.range = range;
+        this.form = form;
+    }
 }
